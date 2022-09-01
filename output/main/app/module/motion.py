aa15d492957e8964923c1eebd4509a892cb39015
Anthony Chan <cachan@my.anlab.cc>
2022-09-01 10:33:27 +0800

    20220901

--- output/main/app/module/motion.py
+++ output/main/app/module/motion.py
@@ -13,10 +13,10 @@ import eel
 #     while True:
 #         coordinate = pyautogui.locateCenterOnScreen(imagePath)
 #         if coordinate:
-#             eel.consoleLog(f'找到 {imagePath} 了 立即點擊')
+#             #eel.consoleLog(f'找到 {imagePath} 了 立即點擊')
 #             break
 #         print(getTime() + ' not found')
-#         eel.consoleLog(f'找不到 {imagePath} 等待一秒後重試')
+#         #eel.consoleLog(f'找不到 {imagePath} 等待一秒後重試')
 #         sleep(1)
 #     x, y = coordinate
 #     pyautogui.moveTo(x, y, duration=duration)
@@ -28,10 +28,10 @@ def touchByPhoto(imagePath, delay=None, duration=0, clicks=1, interval=0, confid
     while True:
         coordinate = pyautogui.locateOnScreen(imagePath, confidence=confidence)
         if coordinate:
-            eel.consoleLog(f'找到 {imagePath} 了 立即點擊')
+            #eel.consoleLog(f'找到 {imagePath} 了 立即點擊')
             break
         print(getTime() + ' not found')
-        eel.consoleLog(f'找不到 {imagePath} 等待一秒後重試')
+        #eel.consoleLog(f'找不到 {imagePath} 等待一秒後重試')
         sleep(1)
     touchByPos(coordinate)
 
@@ -40,51 +40,64 @@ def touchByPhoto(imagePath, delay=None, duration=0, clicks=1, interval=0, confid
 #         sleep(delay)
 #     pyautogui.moveTo(coordinate, duration=duration)
 #     pyautogui.click(coordinate, clicks=clicks, interval=interval)
-#     eel.consoleLog(f'點擊座標 {coordinate}')
+#     #eel.consoleLog(f'點擊座標 {coordinate}')
 
 def touchByPos(coordinate, delay=None, duration=0, clicks=1, interval=0):
     if delay:
         sleep(delay)
     pyautogui.moveTo(coordinate, duration=duration)
     pyautogui.click(coordinate, clicks=clicks, interval=interval)
-    eel.consoleLog(f'點擊座標 {coordinate}')
+    #eel.consoleLog(f'點擊座標 {coordinate}')
 
 
 def getMultiplePos(imagePath, confidence=0.5):
     while True:
         coordinates = list(pyautogui.locateAllOnScreen(imagePath, confidence=confidence, grayscale=False))
         if coordinates:
-            eel.consoleLog(f'找到(多個座標) {imagePath} 了 立即點擊')
-            eel.consoleLog(len(coordinates))
-            eel.consoleLog(coordinates)
+            #eel.consoleLog(f'找到(多個座標) {imagePath} 了 立即點擊')
+            #eel.consoleLog(len(coordinates))
+            #eel.consoleLog(coordinates)
             break
-        eel.consoleLog(f'找不到(多個座標) {imagePath} 等待一秒後重試')
+        #eel.consoleLog(f'找不到(多個座標) {imagePath} 等待一秒後重試')
         print(getTime() + ' not found')
         sleep(1)
     return coordinates
+
+def getPos(imagePath, confidence=0.5):
+    while True:
+        coordinate = list(pyautogui.locateCenterOnScreen(imagePath, confidence=confidence, grayscale=False))
+        if coordinate:
+            #eel.consoleLog(f'找到(多個座標) {imagePath} 了 立即點擊')
+            #eel.consoleLog(len(coordinates))
+            #eel.consoleLog(coordinates)
+            break
+        #eel.consoleLog(f'找不到(多個座標) {imagePath} 等待一秒後重試')
+        print(getTime() + ' not found')
+        sleep(1)
+    return coordinate
 
 
 def isPhotoExist(imagePath):
     # 因為有 judge_multi_com
 
     # sec = 3
-    # eel.consoleLog(f'先睡{str(3)}秒')
+    # #eel.consoleLog(f'先睡{str(3)}秒')
     # sleep(sec)
 
     # flag = pyautogui.locateOnScreen(imagePath)
     flag = pyautogui.locateOnScreen(imagePath, confidence=0.5)
     if flag:
-        eel.consoleLog(f'存在 {imagePath}')
+        #eel.consoleLog(f'存在 {imagePath}')
         return True
-    eel.consoleLog(f'不存在 {imagePath}')
+    #eel.consoleLog(f'不存在 {imagePath}')
     return False
 
 def typeWrite(word):
     pyautogui.typewrite(word)
-    eel.consoleLog(f'貼上文字')
+    #eel.consoleLog(f'貼上文字')
 
 def copePaste(word):
     clipboard.copy(word)
     pyautogui.hotkey("ctrl", "v")
-    eel.consoleLog(f'貼上文字')
+    #eel.consoleLog(f'貼上文字')
 
--- output/main/app/views.py
+++ output/main/app/views.py
@@ -1,8 +1,9 @@
 import app.config as config
 import pyautogui
 import os
-from app.module.motion import touchByPhoto, touchByPos, getMultiplePos,isPhotoExist, copePaste, typeWrite
+from app.module.motion import touchByPhoto, touchByPos,getPos, getMultiplePos,isPhotoExist, copePaste, typeWrite
 from app.module.func import getTime, messageBox
+from app.module.res import Res
 from time import sleep
 from func.db.model.account import selectAllUsers
 import eel
@@ -19,7 +20,9 @@ def preLogin(mode, mobile=None, password=None, coordinate=None):
     # os.startfile(config.main['path'])
     # subprocess.Popen(config.main['path'])'
     os.system(f'start /d "C:\Program Files (x86)\XinStars\\" XinUpdate.exe')
+    
     touchByPhoto(imagePath=config.getImage(img='login'))
+    touchByPhoto(imagePath=config.getImage(img='new_confirm'))
     if mode == 'mobile':
         touchByPhoto(imagePath=config.getImage(mode='mobile', img='item1'))
         touchByPhoto(imagePath=config.getImage(mode='mobile', img='mobile_field'))
@@ -37,13 +40,16 @@ def preLogin(mode, mobile=None, password=None, coordinate=None):
         typeWrite(password)
         touchByPhoto(imagePath=config.getImage(mode='molo', img='login'),confidence=0.9)
         # touchByPhoto(imagePath=config.getImage(mode='mobile', img='item2'))
-
+    sleep(10)
     if not coordinate:
-        coordinates = getMultiplePos(imagePath=config.getImage(mode='mobile', img='login'), confidence=0.9)
-        print(len(coordinates))
+        coordinates = getMultiplePos(imagePath=config.getImage(mode='mobile', img='login'), confidence=0.95)
+        print(f'總共找到:{str(len(coordinates))}個座標')
+        coordinates = coordinates[::-1][::-1]
         count = 1
         for coordinate in coordinates:
-            eel.consoleLog(f'登入 \n 帳號:{mobile} \n 登入方式:{mode}\n 第{ str(count) }隻角色')
+            x, y, c, d = coordinate
+            print(f'點擊 x:{x} y:{y}')
+            #eel.consoleLog(f'登入 \n 帳號:{mobile} \n 登入方式:{mode}\n 第{ str(count) }隻角色')
             if coordinate != coordinates[0]:
                 preLogin(mode, mobile, password, coordinate)
             doLogin(coordinate)
@@ -60,10 +66,10 @@ def doLogin(coordinate):
         sleep(1)
         # 已登入
         if not isPhotoExist(imagePath=config.getImage(img='warn1')):
-            eel.consoleLog(f'沒有被強制退出')
+            #eel.consoleLog(f'沒有被強制退出')
             print(getTime() +'沒有被強制退出')
             break
-        eel.consoleLog(f'被強制退出 等30秒')
+        #eel.consoleLog(f'被強制退出 等30秒')
         print(getTime() +'被強制退出 等30秒')
         sleep(30)
         touchByPhoto(imagePath=config.getImage(img='confirm1'))
@@ -72,10 +78,10 @@ def doLogin(coordinate):
     while True:
         flag = isPhotoExist(imagePath=config.getImage(img='judge_multi_com'))
         if flag:
-            eel.consoleLog(f'找到 judge_multi_com 了')
+            #eel.consoleLog(f'找到 judge_multi_com 了')
             print(getTime() +'找到 judge_multi_com 了')
             break
-        eel.consoleLog(f'還沒找到 judge_multi_com')
+        #eel.consoleLog(f'還沒找到 judge_multi_com')
         print(getTime() +'還沒找到 judge_multi_com')
         sleep(1)
 
@@ -94,22 +100,28 @@ def doLogin(coordinate):
     touchByPhoto(imagePath=config.getImage(img='close'))
     return
 
-def start_auto_control():
+
+def start_auto_control(mode=None):
+    r = Res()
+    r.changeRes(1600, 900)
     users = selectAllUsers()
     for user in users:
         id = user[0]
         username = user[1]
         password = user[2]
         mobile = int(user[3])
-        eel.consoleLog(f'登入 帳號:{username}')
-        if mobile:
-            eel.consoleLog(f'登入方式:立即玩')
-            preLogin('mobile', username, password)
-            eel.consoleLog(f'登入完成')
+        #eel.consoleLog(f'登入 帳號:{username}')
+        if mode!='molo':
+            if mobile:
+                #eel.consoleLog(f'登入方式:立即玩')
+                preLogin('mobile', username, password)
+                #eel.consoleLog(f'登入完成')
 
         molo = int(user[4])
-        if molo:
-            eel.consoleLog(f'登入方式:MOLO')
-            preLogin('molo', username, password)
-            eel.consoleLog(f'登入完成')
-    eel.consoleLog(f'程式已結束運行')
+        if mode!='mobile':
+            if molo:
+                #eel.consoleLog(f'登入方式:MOLO')
+                preLogin('molo', username, password)
+                #eel.consoleLog(f'登入完成')
+    #eel.consoleLog(f'程式已結束運行')
+    r.reductRes()
